5b626a8f73ce5b069cfb72c2191a7774fbfb4171
Bharath34 <34450371+Bharath34@users.noreply.github.com>
2018-01-22 17:13:10 -0500

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,45 @@
+#assiging a varible 
+bd = 10
+print (bd)
+bc= bunny
+print (bc)
+#integer data-type
+a= 20
+print (a)
+#compliex data type
+b= 2+8J
+print(b)
+#float data type
+c=2.35
+print (c)
+#type of convertions
+float to integer
+d=float(a)
+prind (d)
+#integer to float
+e= int(c)
+print(e)
+#list of data-types
+list is a most versinable data type available in python which can be written a list of comma separated values item
+example:- ["a","b","c","d"]
+#step1 define a list
+list1=["ashok","surya","teja","bunny,"] # the index will start from the 0,1,2,3,(number of list )
+#it is substitutab-1
+list1[2]=venkat
+
+print (list1)
+#print the length of the list
+ print(len(list1))
+#print the exact element of list
+w=task(2)
+print (w)
+#teja out put
+#print a part of list
+print(list1(0:3))
+#adding 2 list or concat the tuple
+list["bunny","bunny1","bunny3"]
+
+print (list1+list)
+#print tuple multiple timne
+print (list1*2)
+
